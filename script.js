237ce5dd5ba1c9ecae7f1907e2a281b3612af51e
Matteo0421 <matteo.carola99@gmail.com>
2024-04-18 14:56:40 +0200

    [end]add-card

--- script.js
+++ script.js
@@ -8,11 +8,11 @@ createApp({
       apiUrl: 'server.php',
       list: [],
       newDisk:{
-        poster: 'a',
-        // author:'b',
-        // title:'c',
-        // year:'d',
-        // genre:'e'
+        poster: '',
+        author:'',
+        title:'',
+        year:'',
+        genre:''
       }
     }
   },
@@ -23,6 +23,21 @@ createApp({
         this.list = result.data;
         console.log(this.list);
        })
+    },
+
+    addNewDisk(){
+      const data = new FormData();
+      data.append('newDiskPoster', this.newDisk.poster)
+      data.append('newDiskAuthor', this.newDisk.author)
+      data.append('newDiskTitle', this.newDisk.title)
+      data.append('newDiskYear', this.newDisk.year)
+      data.append('newDiskGenre', this.newDisk.genre)
+
+
+      axios.post(this.apiUrl, data)
+        .then(result =>{
+          this.list = result.data;
+        })
     }
   },
 
--- server.php
+++ server.php
@@ -3,10 +3,18 @@ $json_string = file_get_contents('dischi.json');
 
 $dischi = json_decode($json_string);
 
+if(isset($_POST['newDiskAuthor'])){
+  $new_disk = [
+    'poster' => $_POST['newDiskPoster'],
+    'author' => $_POST['newDiskAuthor'],
+    'title' => $_POST['newDiskTitle'],
+    'year' => $_POST['newDiskYear'],
+    'genre' => $_POST['newDiskGenre'],
 
-
-
-
+  ];
+  $dischi[] = $new_disk;
+  file_put_contents('dischi.json', json_encode($dischi));
+}
 
 
 header('Content-Type: application/json');
